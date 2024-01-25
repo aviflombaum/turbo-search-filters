fb81c1f40d98fa71ebb9f95ffaa70e4bbd609ae2
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2024-01-25 09:24:37 -0500

    Even a little nicer

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,14 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { url: String };
-
   submit(e) {
-    const form = e.currentTarget.closest("form");
+    const form = this.element;
     const formData = new FormData(form);
 
     const params = new URLSearchParams(formData);
-    const newUrl = `${this.urlValue}?${params.toString()}`;
+    const newUrl = `${form.action}?${params.toString()}`;
 
     Turbo.visit(newUrl, { frame: "jobs" });
     history.pushState({}, "", newUrl);
